b7f501cde322b0611474ce48a1f6ea0facd6ad38
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2022-07-15 16:17:24 +0900

    ルーティングの設定を追記

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+  "github.com/gorilla/mux"
+  "log"
+)
 
 func main() {
-	fmt.Println("hello, 世界")
-}
+	// ルーターのイニシャライズ
+	r := mux.NewRouter()
 
+	// ルート(エンドポイント)
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
