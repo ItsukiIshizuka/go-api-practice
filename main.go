1fd7c82ce961fa5d4781327f032a17b348878533
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2022-07-15 16:32:44 +0900

    CRUD関数の中身を記述

--- main.go
+++ main.go
@@ -25,22 +25,68 @@ var books []Book
 
 // Get All Books
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(books)
 }
 
 // Get Single Book
 func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+    params := mux.Vars(r)
+
+    // Loop through books and find with id
+    for _, item := range books {
+        if item.ID == params["id"] {
+            json.NewEncoder(w).Encode(item)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(&Book{})
 }
 
 // Create a Book
 func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+    var book Book
+    _ = json.NewDecoder(r.Body).Decode(&book)
+    book.ID = strconv.Itoa(rand.Intn(10000)) // Mock ID - not safe in production
+    books = append(books, book)
+    json.NewEncoder(w).Encode(book)
 }
 
 // Update a Book
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+    params := mux.Vars(r)
+
+    for index, item := range books {
+        if item.ID == params["id"] {
+            books = append(books[:index], books[index+1:]...)
+            var book Book
+            _ = json.NewDecoder(r.Body).Decode(&book)
+            book.ID = params["id"]
+            books = append(books, book)
+            json.NewEncoder(w).Encode(book)
+            return
+        }
+    }
+    json.NewEncoder(w).Encode(books)
 }
 
 // Delete a Book
-func deleteBook(w http.ResponseWriter, r *http.Request) {\
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+    params := mux.Vars(r)
+    
+    for index, item := range books {
+        if item.ID == params["id"] {
+            books = append(books[:index], books[index+1:]...)
+            break
+        }
+    }
+    json.NewEncoder(w).Encode(books)
 }
 
 func main() {
